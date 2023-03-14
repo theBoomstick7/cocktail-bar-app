34901a4afe08afb4ae603efe6b90143ee69cb25a
theboomstick7 <aleksandur.vartolomeev@gmail.com>
2023-03-14 19:09:14 +0200

    Login and register pages added

--- src/components/auth/Login/Login.js
+++ src/components/auth/Login/Login.js
@@ -1,6 +1,25 @@
+import styles from '../Login/login.module.css'
+
 export const Login = () => {
 
     return ( 
-        <h1>Login PAGE</h1>
+        <form >
+            <div className={styles.login}>
+
+                <h1>Login</h1>
+
+                <label htmlFor="email">Email:</label>
+                <input type="email" placeholder='ivan@mail.bg'/>
+
+                <label htmlFor="password">Password</label>
+                <input type="password" placeholder='********'/>
+
+                <button type='submit'>LOGIN</button>
+           
+            </div>
+
+        </form>
+
+
     )
 }
--- src/components/auth/Register/Register.js
+++ src/components/auth/Register/Register.js
@@ -1,6 +1,28 @@
+import styles from '../Register/register.module.css'
+
 export const Register = () => {
 
     return ( 
-        <h1>REGISTER PAGE</h1>
+        <form >
+            <div className={styles.register}>
+
+            <h1>REGISTER PAGE</h1>
+        
+            <label htmlFor="email">Email:</label>
+            <input type="email" placeholder='ivan@mail.bg'/>
+        
+            <label htmlFor="username">Username</label>
+            <input type="text" placeholder='IvanTheBartender'/>
+
+            
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder='********'/>
+
+            <label htmlFor="repass">Repeat Password</label>
+            <input type="password" placeholder='********'/>
+
+            <button type='submit'>REGISTER</button>
+            </div>
+        </form>
     )
 }
